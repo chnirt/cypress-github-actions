9bdb0edac16438622973b4db93968775a42cc23a
chintrinh-groove <chin.trinh@groovetechnology.com>
2022-09-20 11:27:29 +0700

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,63 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+describe('My First Test', () => {
+  it('finds the content "type"', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type')
+  })
+})
+
+describe('My First Test', () => {
+  it('clicks the link "type"', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+  })
+})
+
+describe('My First Test', () => {
+  it('clicking "type" navigates to a new url', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+  })
+})
+
+describe('My First Test', () => {
+  it('Gets, types and asserts', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it and verify
+    // that the value has been updated
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
+
+describe('My Lighthouse Test', () => {
+  it("should verify the lighthouse scores with thresholds", function () {
+    cy.lighthouse({
+      performance: 64,
+      accessibility: 68,
+      "best-practices": 92,
+      seo: 100,
+      pwa: 20,
+    });
+  });
+})
